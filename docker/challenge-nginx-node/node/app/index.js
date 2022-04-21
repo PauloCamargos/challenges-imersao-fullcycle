86c640c6470d8754a6057b41d877b6cde92d82dc
PauloCamargos <camargosspaulo@gmail.com>
2022-04-21 14:59:20 -0300

    updated app and nginx services

--- docker/challenge-nginx-node/node/app/index.js
+++ docker/challenge-nginx-node/node/app/index.js
@@ -1,9 +1,42 @@
 const express = require('express')
+const mysql = require('mysql')
+
 const app = express()
 const port = 3000
+const config = {
+    connectionLimit: 3,
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+}
+const connectionPool = mysql.createPool(config);
 
 app.get("/", (req, res) => {
-    res.send("<h1>Full CYCLE rocks!</h1>")
+    let template = "<h1>Full cycle rocks!</h1><hr>"
+    connectionPool.getConnection(function(err, connection) {
+        const sql = `INSERT INTO people(name) values('Paulo')`
+        connection.connect()
+        connection.query(sql)
+        connection.release()
+    });
+
+    connectionPool.getConnection(function(err, connection) {
+        connection.query('SELECT `name` FROM `people`', function (error, results) {
+            if (error) {
+                console.log(error);
+            }
+            let i = results.length
+            results.forEach(element => {
+                template += `<p>#${i}: ${element.name}</p>`
+                i -= 1;
+            });
+            connection.release()
+            res.send(template);
+        });
+    })
 })
 
-app.listen(port, () => {})
+app.listen(port, () => {
+    console.log("Running app");
+})
